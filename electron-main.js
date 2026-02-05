ca098a6336d529c2df35c0e179f4708a81d8f165
diego_dell <diegoaprende06@gmail.com>
2026-02-05 14:07:05 -0400

    release 1.0.5

--- electron-main.js
+++ electron-main.js
@@ -16,12 +16,17 @@ fs.mkdirSync(process.env.DB_DIR, { recursive: true });
 let mainWindow;
 
 function createWindow() {
-  mainWindow = new BrowserWindow({
-    width: 1200,
-    height: 800,
-    show: false,
-    autoHideMenuBar: true
-  });
+mainWindow = new BrowserWindow({
+  width: 1200,
+  height: 800,
+  show: false,
+  autoHideMenuBar: true,
+  icon: path.join(__dirname, "assets", "icon.ico"),
+  webPreferences: {
+    contextIsolation: true
+  }
+});
+
 
   mainWindow.loadURL("http://localhost:3000");
   mainWindow.once("ready-to-show", () => mainWindow.show());
